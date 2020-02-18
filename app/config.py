9ea6da372959b4d4b6e835099f07991333da7e51
gaborpelesz <gaborpelesz@hotmail.com>
2020-02-18 10:04:25 +0100

    distance threshold is now in config

--- app/config.py
+++ app/config.py
@@ -1,7 +1,13 @@
 import detection.RetinaFace
 import recognition.InsightFace
 
+# GUI
 WINDOW_TITLE = 'Demo - Face recognizer applicaton'
 IMAGE_SIZE = 200
+
+# Face recognition
 DETECTOR = detection.RetinaFace.RetinaFace()
 RECOGNIZER = recognition.InsightFace.InsightFace()
+
+# Face recognition parameters
+UNKNOWN_DISTANCE_THRESHOLD = 320
